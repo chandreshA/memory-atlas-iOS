a279f2c33961169730d9d940b66c109ac7e759e8
Chandresh Ahir <cbahir1996@gmail.com>
2026-09-22 19:57:12 -0400

    test with mock

--- MemoryAtlas/View/TimelineView.swift
+++ MemoryAtlas/View/TimelineView.swift
@@ -10,19 +10,24 @@ import SwiftUI
 struct TimelineView: View {
     private var memory: [Memory]
 
-    init(memory: [Memory]) {
+    init(memory: [Memory] = [Memory(id: 1, title: "some title", note: "adding notes", memoryDate: Date(), creationDate: Date()), Memory(id: 2, title: "some title", note: "adding notes", memoryDate: Date(), creationDate: Date()), Memory(id: 3, title: "some title3", note: "adding notes", memoryDate: Date(), creationDate: Date())]) {
         self.memory = memory
     }
 
     var body: some View {
         List {
             ForEach(memory) { memory in
-                EmptyView()
+                VStack(alignment: .leading) {
+                    Text(memory.title)
+                        .font(.headline)
+                    Text(memory.note)
+                        .font(.subheadline)
+                }
             }
         }
     }
 }
 
 #Preview {
-    TimelineView(memory: [])
+    TimelineView()
 }
